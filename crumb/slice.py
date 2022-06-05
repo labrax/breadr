cbafe45ee0166712c7e7aabc484989713505afea
Victor R. Cardoso <labrax@users.noreply.github.com>
2022-06-05 22:52:28 +0100

    slice: internal name of crumb used stored in all.nodes

--- crumb/slice.py
+++ crumb/slice.py
@@ -20,8 +20,8 @@ class Slice:
         self.output = dict() # {'name': <type>}
         self._output_mapping = dict() # {'output_name': ('node name', 'Node output name'])} # an output can com from a single bakery_item
         
-        self.crumbs = dict() # {'name given': {'crumb': crumb, 'is_used': bool}}
-        self.nodes = dict() # {'identifier': Node}
+        self.crumbs = dict() # {'name given': {'crumb': crumb}}
+        self.nodes = dict() # {'identifier': {'node': Node, 'type': 'slice crumb's name'}}
 
         self._graph_checked = False
         # these are the nodes that require input, if _graph_checked is True, they are in _input_mapping format is {node: {'node var': 'node var type'}}
@@ -33,12 +33,12 @@ class Slice:
     def add_crumb(self, name, crumb):
         if name in self.crumbs:
             raise ValueError(f'crumb "{name}" already in Slice')
-        self.crumbs[name] = {'crumb': crumb,
-                             'is_used': False}
+        self.crumbs[name] = {'crumb': crumb}
 
     def add_node(self, crumb_name):
         n = Node(self.crumbs[crumb_name]['crumb'])
-        self.nodes[n.name] = n
+        self.nodes[n.name] = {'node': n,
+                              'type': crumb_name}
         return n.name
 
     def remove_node(self, node_name):
@@ -51,7 +51,7 @@ class Slice:
                 if node_name in i:
                     raise RuntimeError(f'cannot remove "{node_name}" as it is linked to output')
 
-        if self.nodes[node_name].has_links():
+        if self.nodes[node_name]['node'].has_links():
             raise RuntimeError(f'cannot remove "{node_name}" as it is connect in the graph')
         
         self.nodes.pop(node_name)
@@ -90,10 +90,10 @@ class Slice:
         if node_name not in self.nodes:
             raise RuntimeError(f'"{node_name}" not in slice')
         if node_input is not None:
-            if node_input not in self.nodes[node_name].input.keys():
+            if node_input not in self.nodes[node_name]['node'].input.keys():
                 raise RuntimeError(f'"{node_input}" not in "{node_name}" input list')
         if node_output is not None:
-            if node_output not in self.nodes[node_name].output.keys():
+            if node_output not in self.nodes[node_name]['node'].output.keys():
                 raise RuntimeError(f'"{node_output}" not in "{node_name}" output list')
 
     def _check_graph_circular(self):
@@ -109,7 +109,7 @@ class Slice:
             stack = [stack_start]
             while len(stack) > 0:
                 current = stack.pop(0)
-                for el in self.nodes[current].output.values(): # for each {'output name': {node, ['input name', ...]}}
+                for el in self.nodes[current]['node'].output.values(): # for each {'output name': {node, ['input name', ...]}}
                     for out_node, _ in el.items(): # for each {node, [...]}
                         if (out_node.name in visited) and (visited[out_node.name] == it):
                             raise RuntimeError(f'"{out_node.name}" already explored, graph is circular')
@@ -121,7 +121,7 @@ class Slice:
 
     def get_nodes_missing_input(self, only_in_output=True):
         if only_in_output:
-            stack = list({self.nodes[node] for node, _ in self._output_mapping.values()})
+            stack = list({self.nodes[node]['node'] for node, _ in self._output_mapping.values()})
         else:
             stack = self.nodes
 
@@ -149,7 +149,7 @@ class Slice:
         # remove the ones that will be given on the run call
         for _, data in self._input_mapping.items():
             for node_name, vars in data.items():
-                node = self.nodes[node_name]
+                node = self.nodes[node_name]['node']
                 if node in input_undefined:
                     for i in vars:
                         input_undefined[node].pop(i)
@@ -181,8 +181,8 @@ class Slice:
         nodes_seq = [] # format is: [{'node': node_id, 'deps': [node_id_1, node_id_2, ...]}]
         # start from slice input
         for node in self.nodes.values():
-            this = {'node': node, 'deps': []}
-            for _, data in node.input.items():
+            this = {'node': node['node'], 'deps': []}
+            for _, data in node['node'].input.items():
                 if not data is None: # if none comes from slice!
                     this['deps'].append(data[0].name)
             nodes_seq.append(this)
@@ -218,7 +218,8 @@ class Slice:
             print('results of slice execution are:', results)
 
         # populated nodes with output if needed
-        for node in self.nodes:
+        for node in self.nodes.values():
+            node = node['node']
             if node in self.last_execution_seq: # node was executed
                 if not node.name in results:
                     raise RuntimeError('expected result to be in here but is not') #TODO: possibly this can become a warning
@@ -275,6 +276,7 @@ class Slice:
         self._check_output_exists(name, check_mapping=True) # if doesn't exist or in mapping not good to remove
         self.output.pop(name)
         self._output_mapping.pop(name)
+    #
 
     # mapping functions
     def add_input_mapping(self, name, node_name, node_input):
@@ -286,8 +288,10 @@ class Slice:
         """
         self._check_input_exists(name, check_mapping=False)
         self._check_node_exists(node_name, node_input)
-        if self.input[name] != self.nodes[node_name].get_input_type(node_input):
-            raise RuntimeError(f'"{name}" has got different type than input for node "{node_name}" (input: {node_input}). types are: "{self.input[name]}" and "{self.nodes[node_name].input[node_input]}"')
+
+        node = self.nodes[node_name]['node']
+        if self.input[name] != node.get_input_type(node_input):
+            raise RuntimeError(f'"{name}" has got different type than input for node "{node_name}" (input: {node_input}). types are: "{self.input[name]}" and "{node.input[node_input]}"')
         if not name in self._input_mapping:
             self._input_mapping = dict()
         if not node_name in self._input_mapping[name]:
@@ -318,8 +322,9 @@ class Slice:
         """
         self._check_output_exists(name, check_mapping=True)
         self._check_node_exists(node_name, node_output)
-        if self.output[name] != self.nodes[node_name].get_output_type(node_output):
-            raise RuntimeError(f'"{name}" has got different type than output for node "{node_name}" (output {node_output}). types are: "{self.output[name]}" and "{self.nodes[node_name].output[node_output]}"')
+        node = self.nodes[node_name]['node']
+        if self.output[name] != node.get_output_type(node_output):
+            raise RuntimeError(f'"{name}" has got different type than output for node "{node_name}" (output {node_output}). types are: "{self.output[name]}" and "{node.output[node_output]}"')
         if self._output_mapping[name] is None:
             self._output_mapping[name] = (node_name, node_output)
 
@@ -339,6 +344,7 @@ class Slice:
         self._graph_checked = False
     #
 
+    # link
     def add_link(self, nodeA, nodeA_output, nodeB, nodeB_input):
         """
         Links two nodes
@@ -349,19 +355,20 @@ class Slice:
         """
         self._check_node_exists(nodeA, node_output=nodeA_output)
         self._check_node_exists(nodeB, node_input=nodeB_input)
-        A = self.nodes[nodeA]
-        B = self.nodes[nodeB]
+        A = self.nodes[nodeA]['node']
+        B = self.nodes[nodeB]['node']
         A.add_output(this_output_name=nodeA_output, other_node=B, other_node_variable=nodeB_input)
         B.add_input(this_variable=nodeB_input, other_node=A, other_node_name=nodeA_output)
 
     def remove_link(self, nodeA, nodeA_output, nodeB, nodeB_input):
         self._check_node_exists(nodeA, node_output=nodeA_output)
         self._check_node_exists(nodeB, node_input=nodeB_input)
-        A = self.nodes[nodeA]
-        B = self.nodes[nodeB]
+        A = self.nodes[nodeA]['node']
+        B = self.nodes[nodeB]['node']
         A.remove_output(this_output_name=nodeA_output, other_node=B, other_node_variable=nodeB_input)
         B.remove_input(this_variable=nodeB_input)
         self._graph_checked = False
+    #
 
     def __repr__(self):
         return f'{self.__class__.__name__} at {hex(id(self))} with {len(self.crumbs)} crumbs'
@@ -369,6 +376,7 @@ class Slice:
     def __str__(self):
         return self.__repr__()    
 
+    # json save load
     def to_json(self):
         this_structure = {
             'slice_name': self.name,
@@ -382,8 +390,7 @@ class Slice:
                 'mapping': self._output_mapping
             },
             'crumbs': {i: {
-                'crumb': j['crumb'].to_json(), 
-                'is_used': j['is_used']
+                'crumb': j['crumb'].to_json()
             } for i, j in self.crumbs.items()} if self.crumbs else {}
         }
         return json.dumps(this_structure)
@@ -401,8 +408,7 @@ class Slice:
         self._output_mapping = json_str['output']['mapping']
 
         self.crumbs = {i:{
-            'crumb': Crumb.create_from_json(j['crumb']), 
-            'is_used': j['is_used']
+            'crumb': Crumb.create_from_json(j['crumb'])
         } for i, j in json_str['crumbs'].items()} if json_str['crumbs'] else {}
 
     def save(self, path, overwrite=False):

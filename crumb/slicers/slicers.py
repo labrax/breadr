3e4ca56f6354e7f68f259525b8066d26f1cbf1b2
Victor R. Cardoso <labrax@users.noreply.github.com>
2022-06-06 03:54:13 +0100

    slicers.py delete_slicer added and comments

--- crumb/slicers/slicers.py
+++ crumb/slicers/slicers.py
@@ -4,6 +4,10 @@ from crumb.slicers.multislicer import MultiSlicer
 from crumb.slicers.singleslicer import SingleSlicer
 
 def get_slicer():
+    """
+    Returns the executor for the nodes.
+    Depending on the settings, this could either be multi or single process.
+    """
     global task_executor_instance
     try: task_executor_instance
     except NameError:
@@ -12,3 +16,14 @@ def get_slicer():
         else:
             task_executor_instance = SingleSlicer() # inside there is another singleton
     return task_executor_instance
+
+def delete_slicer():
+    """
+    Deletes the current slicer.
+    This can be used to reset the slicer and start with another one (after changing settings)
+    """
+    global task_executor_instance
+    try: 
+        del task_executor_instance
+    except NameError:
+        pass

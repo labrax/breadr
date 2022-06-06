75293741dffe7a698778ade0eb3ef7e4d873de1b
Victor R. Cardoso <labrax@users.noreply.github.com>
2022-06-06 03:58:39 +0100

    settings more comments!

--- crumb/settings.py
+++ crumb/settings.py
@@ -1,6 +1,7 @@
 
 debug = False
-# if started as single, then multi, then single it might break depending where the functions come from!
+# if started as single, then exec as multi, then changed to single it might break depending where the functions come from!
+# if the functions come from top level of a file it will work
 multislicer = False
 multislicer_wait_worker_delay = .5
 multislicer_threads = 4

f8c6e34694fa0d6c08d97a07feab62f7d28ef1a5
Victor R. Cardoso <labrax@users.noreply.github.com>
2022-06-19 07:59:55 +0100

    validate settings passed and convert if possible

--- src/crumb/settings.py
+++ src/crumb/settings.py
@@ -21,10 +21,45 @@ class Settings:
     MULTISLICER_START_THEN_KILL_THREADS = False
 
     @classmethod
+    def is_int(cls, value):
+        """Returns True if the value is int"""
+        try:
+            int(value)
+            return True
+        except ValueError:
+            return False
+
+    @classmethod
+    def is_float(cls, value):
+        """Returns True if the value is float"""
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+
+    @classmethod
+    def is_bool(cls, value):
+        """Returns True if the value is bool"""
+        try:
+            bool(value)
+            return True
+        except ValueError:
+            return False
+
+    @classmethod
     def set_setting(cls, setting: str, new_value: Any) -> None:
         """Set a setting into a new value if valid"""
         if hasattr(cls, setting):
-            if isinstance(getattr(cls, setting), type(new_value)):
+            # check for some builtin types
+            if isinstance(getattr(cls, setting), int) and cls.is_int(new_value):
+                setattr(cls, setting, int(new_value))
+            elif isinstance(getattr(cls, setting), float) and cls.is_float(new_value):
+                setattr(cls, setting, float(new_value))
+            elif isinstance(getattr(cls, setting), bool) and cls.is_bool(new_value):
+                setattr(cls, setting, bool(new_value))
+            # check for str
+            elif isinstance(getattr(cls, setting), type(new_value)):
                 setattr(cls, setting, new_value)
             else:
                 raise TypeError(f'Invalid value for setting "{setting}", got "{type(new_value)}" expected "{type(getattr(cls, setting))}"')

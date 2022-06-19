99d39fe97bd5e80b695f89f788f1af278a9b43c9
Victor R. Cardoso <labrax@users.noreply.github.com>
2022-06-19 08:01:38 +0100

    execute a breadr file

--- src/crumb/__main__.py
+++ src/crumb/__main__.py
@@ -1,14 +1,53 @@
-
+"""Breadr main executer"""
+from multiprocessing.sharedctypes import Value
 import sys
 import argparse
+import json
+import pprint
 
-import settings as settings
+from .settings import Settings
+from .bakery_items.slice import Slice
+
 
 def main():
+    """Main function"""
     parser = argparse.ArgumentParser(prog='breadr', usage='%(prog)s [options]')
-    parser.add_argument('show', type=str, help='displays information about a slice file')
-    parser.add_argument('run', type=str, help='executes a slice file')
-    parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+    parser.add_argument('file', type=str, help='Slice file')
+    parser.add_argument('-show', help='displays information about a slice file', action='store_true')
+    parser.add_argument('-run', help='executes a slice file', action='store_true')
+    parser.add_argument('-input', type=str, help='set input parameters, use comma separated values with equals')
+    parser.add_argument('-setting', type=str, help='set some settings, use comma separated values with equals')
+    arguments = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+    # set settings before running
+    if arguments.setting:
+        for i in arguments.setting.split(','):
+            setting, value = i.split('=')
+            Settings.set_setting(setting, value)
+    # load the file and show some info
+    if arguments.show:
+        with open(arguments.file, mode='r', encoding='utf-8') as open_file:
+            pprint.pprint(json.loads(open_file.read()))
+    # compile input bits, split , then split =
+    input = {}
+    if arguments.input:
+        for i in arguments.input.split(','):
+            this_input, value = i.split('=')
+            input[this_input] = value
+    # show time!
+    if arguments.run:
+        slice = Slice('main_exec')
+        slice.load_from_file(arguments.file)
+        for slice_input, value in input.items():
+            if slice_input in slice.input:
+                type = slice.input[slice_input]
+                try:
+                    input[slice_input] = type(input[slice_input])
+                except ValueError as exc:
+                    raise ValueError(f'Invalid type for "{slice_input}", expected "{type.__name__}".') from exc
+        ret = slice.run(input)
+        pprint.pprint(ret)
+
 
 if __name__ == '__main__':
     main()

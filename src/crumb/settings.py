734f1f11edef99e70650fc7e721a40a36e3909b9
Victor R. Cardoso <labrax@users.noreply.github.com>
2022-06-19 07:07:40 +0100

    added details on Settings issue

--- src/crumb/settings.py
+++ src/crumb/settings.py
@@ -15,7 +15,7 @@ class Settings:
     # if started as single, then exec as multi, then changed to single it might break depending where the functions come from!
     # if the functions come from top level of a file it will work
     USE_MULTISLICER = False
-    MULTISLICER_WAITWORKER_DELAY = .1  # TODO: this setting might not work
+    MULTISLICER_WAITWORKER_DELAY = .1  # TODO: this setting might not work - it will be obtained by other processes but cannot be set from another one
     MULTISLICER_THREADS = 4
     # if atexit does not work properly it will be required to manually ask the threads to exit!
     MULTISLICER_START_THEN_KILL_THREADS = False
